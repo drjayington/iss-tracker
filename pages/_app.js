e7ec26b284494fe88ef642e51254e983311598af
drjayington <jasonp85@hotmail.co.uk>
2020-06-19 11:47:48 +0100

    project at basic create-react-app state

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/index.scss";
-
-const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps}></Component>;
-};
-
-export default MyApp;
